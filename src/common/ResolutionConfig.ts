232731dd836c0463f49c9953385932b68e078b5a
JingFeng <gejinnian224@outlook.com>
2019-03-03 11:33:52 +0800

    分辨率适配更新

--- src/common/ResolutionConfig.ts
+++ src/common/ResolutionConfig.ts
@@ -21,31 +21,28 @@ module resolution {
 
         public setResoulution() {
             let autoscale = Laya.stage.scaleMode;
+            let ratio = 1;
             if (autoscale == "fixedwidth") {
                 //适应宽度，此时舞台宽度跟浏览器宽度是等价的
                 //1个坐标单位 等价于多少个物理像素
-                var unit = Laya.Browser.clientWidth / Laya.stage.designWidth;
-                //舞台实际暂用的物理像素个数
-                var realHeight = Laya.stage.designHeight * unit;
+                ratio = Laya.Browser.clientWidth / Laya.stage.designWidth;
                 //黑边暂用的物理像素的个数
-                var blackRealHeight = Laya.Browser.clientHeight - realHeight;
+                var blackRealHeight = Laya.Browser.clientHeight/ratio - Laya.stage.designHeight;
                 // Laya.stage.y += blackRealHeight/2/unit;
-                ResolutionConfig.diffHight = blackRealHeight / 2 / unit;
+                ResolutionConfig.diffHight = blackRealHeight / 2;
             }
             else if (autoscale == "fixedheight") {
                 //适应宽度，此时舞台宽度跟浏览器宽度是等价的
                 //1个坐标单位 等价于多少个物理像素
-                var unit = Laya.Browser.clientHeight / Laya.stage.designHeight;
-                //舞台实际暂用的物理像素个数
-                var realWidth = Laya.stage.designWidth * unit;
+                ratio = Laya.Browser.clientHeight / Laya.stage.designHeight;
                 //黑边暂用的物理像素的个数
-                var blackRealWidth = Laya.Browser.clientWidth - realWidth;
+                var blackRealWidth = Laya.Browser.clientWidth/ratio - Laya.stage.designWidth;
                 // Laya.stage.x += blackRealWidth/2/unit;
-                ResolutionConfig.diffWidth = blackRealWidth / 2 / unit;
+                ResolutionConfig.diffWidth = blackRealWidth / 2;
             }    
-
-            ResolutionConfig.realWidth += Laya.stage.width + 2*ResolutionConfig.diffWidth;
-            ResolutionConfig.realHeight += Laya.stage.height + 2*ResolutionConfig.diffHight;        
+            //坐标宽高 等于分辨率/缩放系数
+            ResolutionConfig.realWidth = Laya.Browser.clientWidth / ratio;
+            ResolutionConfig.realHeight = Laya.Browser.clientHeight / ratio;        
         }
     }
 }
